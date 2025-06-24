2d5b522126879348aac2556dc279fcfddd1f4a8c
Willie <liaowillie44@gmail.com>
2025-06-24 12:00:45 +0800

    init as well

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wavewave/', include('home.urls')),
+    path('', include('home.urls')),
     path("admin/", admin.site.urls),
     path("", include('admin_volt.urls')),
 ]
--- home/urls.py
+++ home/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('ismartwave/', views.ismartwave_dashboard, name='ismartwave'),
     path('api/get_dashboard_ws/', views.dashboard_ws, name='get_dashboard_ws'),
-    
-    # 新增的路由名稱，避免衝突
-    path('radar-monitor/', views.dashboard, name='radar_monitor'),
-    path('wave-dashboard/', views.dashboard, name='wave_dashboard'),
-    path('smart-radar/', views.ismartwave_dashboard, name='smart_radar'),
 ]
