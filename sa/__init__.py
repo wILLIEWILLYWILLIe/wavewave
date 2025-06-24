5f5cdf52b64728c9e372744beaabf03119b4875b
Willie <liaowillie44@gmail.com>
2025-06-24 11:54:25 +0800

    raw commit for UI develop

--- sa/__init__.py
+++
@@ -1,1 +0,0 @@
-from .sa_manager import SaManager
--- sa/sa_setting.py
+++
@@ -1,8 +0,0 @@
-from .smdevice.sm_api import *
-from enum import Enum
-from dataclasses import dataclass
-
-class SA_TRIGGER_TYPE(str, Enum):
-    RISING = 'rising'
-    FALLING = 'falling'
-    IMMEDIATE = 'immediate'
